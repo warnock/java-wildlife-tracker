a580968a5ec9f2840fcd38556d533a81be21f8d2
Megan Warnock <megandwarnock@gmail.com>
2017-04-08 19:29:14 -0700

    implement interface methods into Ranger class

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -8,15 +8,26 @@ public class Ranger implements DatabaseManagement {
 
   public Ranger(String name, int badge_number, String address) {
     this.name = name;
-    this.badgeNumber = badge_number;
+    this.badge_number = badge_number;
     this.address = address;
   }
 
 
 
+  @Override
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO rangers (name, badge_number, address) VALUES (:name, :badge_number, :address);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("badge_number", this.badge_number)
+        .addParameter("address", this.address)
+        .executeUpdate()
+        .getKey();
+    }
+  }
 
-
-
+  @Override
   public void delete() {
     try(Connection con = DB.sql2o.open()) {
       String sql = "DELETE FROM rangers WHERE id=:id;";
